7d120b938237ee252bbc6731d010a3dd3e4b0b34
Bằng Phạm <80609005+bangpc147@users.noreply.github.com>
2023-01-03 08:26:06 +0700

    insert values

--- GameTest.sql
+++ GameTest.sql
@@ -5,7 +5,7 @@ Go
 set ansi_nulls on
 go
 create table [dbo].[game](
-[ProductID] [int] identity(1,1) not null,
+[ProductID] [int] identity(1,1),
 [Game_Type] [int] null,
 [Name] [nvarchar](250) null,
 [Price] [float] null,
@@ -26,7 +26,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[loaigame](
-	[Game_Type] [int] IDENTITY(1,1) NOT NULL,
+	[Game_Type] [int] IDENTITY(1,1),
 	[Type_Name] [nvarchar](250) NULL,
 PRIMARY KEY CLUSTERED 
 (
@@ -40,7 +40,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Account](
-	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[UserID] [int] IDENTITY(1,1),
 	[UserName] [nvarchar](500) NULL,
 	[Phone] [nvarchar](10) NULL,
 	[PassWord] [nvarchar](100) NULL,
@@ -57,7 +57,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[CTDonHang](
-	[Ma_CTDH] [int] IDENTITY(1,1) NOT NULL,
+	[Ma_CTDH] [int] IDENTITY(1,1),
 	[Ma_DH] [int] NULL,
 	[ProductID] [int] NULL,
 	[So_Luong] [int] NULL,
@@ -75,7 +75,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Don_Hang](
-	[SoDH] [int] IDENTITY(1,1) NOT NULL,
+	[SoDH] [int] IDENTITY(1,1),
 	[UserID] [int] NULL,
 	[Ngay_Mua] [datetime] NULL,
 	[Tinh_Trang_Don_Hang] [bit] NULL,
@@ -119,3 +119,27 @@ select [ProductID]
 where Game_Type=@maloai
 GO
 /* */
+
+insert into game values ('1000','fps','Left 4 dead 2','120000','','3.5','là một trò chơi bắn súng góc nhìn thứ nhất','1','PC');
+insert into game values ('null','fps','Counter-strike Global Offensive','350000','','4.5','là một trò chơi bắn súng fps','1','PC');
+insert into game values ('null','Third-Person shooter','Risk of rain 2','180000','','4.0','bắn súng góc nhìn thứ ba giống người thật được phát triển bởi Hopoo Games','1','PC');
+insert into game values ('null','Simulation','GTA V','500000','','3.8','Grand Theft Auto V là một trò chơi điện tử hành động phiêu lưu năm 2013','1','PC');
+insert into game values ('null','Simulation','Hero Siege','120000','','3.2','Hero Siege là một trò chơi Slash của Hack với các yếu tố roguelike- & RPG','0','Console');
+insert into game values ('null','FPS','Rainbow Six Siege','300000','','3.5','là một trò chơi điện tử bắn súng chiến thuật','0','Console');
+insert into game values ('null','Racing','Mario Kart Wii','90000','','5','là một trò chơi đua xe kart được phát triển và xuất bản bởi Nintendo cho Wii','0','Console');
+insert into game values ('null','Simulation','Pokémon Unite','12000','','4',' là một trò chơi điện tử đấu trường trực tuyến nhiều người chơi','0','Console');
+insert into game values ('null','fps','Left 4 dead 2','120000','','3.5','là một trò chơi bắn súng góc nhìn thứ nhất','0','Console');
+insert into game values ('null','fps','Left 4 dead 2','120000','','3.5','là một trò chơi bắn súng góc nhìn thứ nhất','0','Console');
+
+insert into loaigame values ('1','fps');
+insert into loaigame values ('null','Third-Person shooter');
+insert into loaigame values ('null','Simulation');
+insert into loaigame values ('null','Racing');
+insert into loaigame values ('null','PVP');
+
+insert into Account values ('1','admin','036182673','123456','lmao@gmail.com');
+insert into Account values ('null','phuc','036182673','123456','lmao@gmail.com');
+
+insert into Don_Hang values ('1','2','3/5/2022','1');
+
+insert into CTDonHang values ('1','12354','3971273','1','120000','120000');
